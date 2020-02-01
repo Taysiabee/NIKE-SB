75ab6240686803d5f486d6ce52eeec3969e3e690
Taysiabee <Taysiabee@yahoo.com>
2020-02-01 17:09:28 -0500

    trends

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,50 @@
-// document.querySelector('carousel-arrow-next').addEventListener('click',carouselSwipe,false);
+let blueDeck = document.querySelector(".blue-deck");
+let brownDeck = document.querySelector(".brown-deck");
+let pinkDeck = document.querySelector(".pink-deck");
 
-// document.querySelector('carousel-arrow-prev').addEventListener('click',carouselSwipe,false);
+if (blueDeck && brownDeck && pinkDeck) {
+blueDeck.addEventListener('click', function(event){
+		blueDeck.classList.add("selected");
+		brownDeck.classList.remove("selected");
+		pinkDeck.classList.remove("selected");
+})
+
+brownDeck.addEventListener('click', function(event){
+		brownDeck.classList.add("selected");
+		blueDeck.classList.remove("selected");
+		pinkDeck.classList.remove("selected");
+})
+
+pinkDeck.addEventListener('click', function(event){
+		pinkDeck.classList.add("selected");
+		blueDeck.classList.remove("selected");
+		brownDeck.classList.remove("selected");
+})
+
+}
+
+
+let purpleWheel = document.querySelector(".purple-wheel");
+let pinkWheel = document.querySelector(".pink-wheel");
+let brownWheel = document.querySelector(".brown-wheel");
+
+if (purpleWheel && pinkWheel && brownWheel) {
+purpleWheel.addEventListener('click', function(event){
+		purpleWheel.classList.add("selected");
+		pinkWheel.classList.remove("selected");
+		brownWheel.classList.remove("selected");
+})
+
+pinkWheel.addEventListener('click', function(event){
+		pinkWheel.classList.add("selected");
+		purpleWheel.classList.remove("selected");
+		brownWheel.classList.remove("selected");
+})
+
+brownWheel.addEventListener('click', function(event){
+		brownWheel.classList.add("selected");
+		pinkWheel.classList.remove("selected");
+		purpleWheel.classList.remove("selected");
+})
+
+}
